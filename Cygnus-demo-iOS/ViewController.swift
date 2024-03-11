a44727a7de2e266946ae5f1ce856ecbc722fab4e
Enchan <enchant0130@gmail.com>
2024-03-11 17:14:45 +0900

    [Add] iOS default app project

---
+++ Cygnus-demo-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cygnus-demo-iOS
+//
+//  Created by EnchantCode on 2024/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
