ce6f18ae0cf802da663941cf16d0a61a81703943
Enchan <enchant0130@gmail.com>
2024-03-11 17:04:21 +0900

    [Add] macOS default app project

---
+++ Cygnus-demo-macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Cygnus-demo-macOS
+//
+//  Created by EnchantCode on 2024/03/11.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
