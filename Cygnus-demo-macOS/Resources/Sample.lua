c3aa253fafb1d93821b03fe453295299ba8bcd0d
Enchan <enchant0130@gmail.com>
2024-03-15 10:35:58 +0900

    [Modify #45] sample source

--- Cygnus-demo-macOS/Resources/Sample.lua
+++ Cygnus-demo-macOS/Resources/Sample.lua
@@ -4,13 +4,6 @@
 -- @2024 Enchan1207.
 --
 
--- This is setup function. Run once.
-function setup()
-    size(400, 400)
-    fill("#FF0000")
-    stroke("#0000")
-end
-
 -- 矩形の位置とサイズ
 x = 200
 y = 200
@@ -18,16 +11,27 @@ w = 20
 h = 20
 
 -- 移動速度
-vx = (math.random() + 1.0) * 2.0
-vy = (math.random() + 1.0) * 2.0
+vx = 0
+vy = 0
+
+-- This is setup function. Run once.
+function setup()
+    size(400, 400)
+    fill("#FF0000")
+    stroke("#0000")
+
+    -- 移動速度の設定
+    vx = getInitialVelocity()
+    vy = getInitialVelocity()
+end
 
 -- This is main loop. Run repeatedly.
 function draw()
     -- 背景を塗りつぶす
-    background("#ccc")
+    background("#444")
 
-    -- 矩形を描画する
-    rect(x - w / 2, y - h / 2, w, h)
+    -- 円を描画する
+    ellipse(x - w / 2, y - h / 2, w, h)
 
     -- 速度に従って移動する ぶつかったら跳ね返る
     x = x + vx
@@ -38,4 +42,17 @@ function draw()
     if(y < h / 2 or y > (400.0 - h / 2)) then
         vy = -vy
     end
+end
+
+-- 初期速度を生成する
+function getInitialVelocity()
+    -- -1.0 ~ +1.0
+    local base = (math.random() - 0.5) * 2.0
+
+    if base > 0 then
+        base = base + 0.5
+    else
+        base = base - 0.5
+    end
+    return base
 end
